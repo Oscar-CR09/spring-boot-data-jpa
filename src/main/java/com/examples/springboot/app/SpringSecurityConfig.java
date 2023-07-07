02fcbbb1de7f3f2dd9773e86e5f43e68c1b8cd0b
Oscar <oscarcr189@outlook.com>
2023-07-06 23:18:29 -0600

    Se actualiza con security spring agregando opciones de la pagina oficial agregando logginControler revisar

--- src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.examples.springboot.app;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
@@ -20,57 +20,36 @@ public class SpringSecurityConfig {
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-    @Bean
-    public UserDetailsService userDetailsService()throws Exception{
-                    
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User
-               .withUsername("oscar")
-               .password(passwordEncoder().encode("12345"))
-               .roles("USER")
-               .build());
- 
-        manager.createUser(User
-               .withUsername("admin")
-               .password(passwordEncoder().encode("admin"))
-               .roles("ADMIN","USER")
-               .build());
-            
-        return manager;
-    }
-    
-    @Controller
-    public class MyController {
 
-        @GetMapping("/login")
-        public String login() {
-            return "login";
-        }
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
 
-    }
-    
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) ->  {
-                try {
-                    authz.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-                        .requestMatchers("/uploads/**").hasRole("USER")
-                        .requestMatchers("/ver/**").hasRole("USER")
-                        .requestMatchers("/factura/**").hasRole("ADMIN")
-                        .requestMatchers("/form/**").hasRole("ADMIN")
-                        .requestMatchers("/eliminar/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                        .anyRequest().denyAll();
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-            }
-            	
-            		);
- 
-        return http.build();
-            
-    }
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withUsername("oscar").password(passwordEncoder().encode("12345")).roles("USER").build());
+
+		manager.createUser(
+				User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER").build());
+
+		return manager;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorize) -> 
+			
+				authorize
+				.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+						.requestMatchers("/uploads/**").hasRole("USER").requestMatchers("/ver/**").hasRole("USER")
+						.requestMatchers("/factura/**").hasRole("ADMIN").requestMatchers("/form/**").hasRole("ADMIN")
+						.requestMatchers("/eliminar/**").hasRole("ADMIN").requestMatchers("/login").authenticated()
+						.anyRequest().denyAll()
+					
+						        
+		
+					);
+
+		return http.build();
+
+	}
 }
--- src/main/java/com/examples/springboot/app/controllers/LogginController.java
+++ src/main/java/com/examples/springboot/app/controllers/LogginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class LogginController {
 
-	@GetMapping("/loggin")
+	@GetMapping("/login")
 	public String login(Model model, Principal principal,RedirectAttributes flash) {
 		
 		if (principal != null) {
