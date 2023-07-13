5d6b95e7cee93f51f3fe80476cda5b2d3783be38
Oscar <oscarcr189@outlook.com>
2023-07-12 23:28:42 -0600

    Se actualiza con security spring con jdbc y mysql se agrego archivos de boostrap

--- src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/examples/springboot/app/SpringSecurityConfig.java
@@ -1,18 +1,18 @@
 package com.examples.springboot.app;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
@@ -25,76 +25,68 @@ public class SpringSecurityConfig {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
 	
-	@Autowired
-	@Bean
-	public UserDetailsService userDetailsService() throws Exception {
-
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(
-				User.withUsername("oscar").password(passwordEncoder().encode("12345")).roles("USER").build());
-
-		manager.createUser(
-				User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER").build());
-
-		return manager;
+    @Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		build.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select usuario, passoword, enabled from usuarios where usuario=?")
+		.authoritiesByUsernameQuery("select u.usuario, a.authority from authorities a inner join usuarios u on (a.user_id=u.id) where u.usuario=?");
+		
 	}
 	@Configuration
 	@EnableWebSecurity
 	public class SecurityConfig {
 
-	    @Bean
-	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	        http
-	            .authorizeHttpRequests(authorize -> authorize
-	                .requestMatchers("\"/\", \"/css/**\", \"/js/**\", \"/images/**\", \"/listar\"").permitAll()
-	                .anyRequest().authenticated()
-	            )
-	            .formLogin(formLogin -> formLogin
-	            	.successHandler(successHandler)
-	                .loginPage("/login")
-	                
-	                .permitAll()
-	            )
-	            
-	            .exceptionHandling((exceptionHandling) -> exceptionHandling
-						.accessDeniedPage("/error_403"))
-	            
-	            .logout((logout) -> logout.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
-	            //.logout((logout) -> logout.logoutUrl("/my/success/endpoint"))
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+			http.authorizeHttpRequests(authorize -> authorize
+					.requestMatchers("\"/\", \"/css/**\", \"/js/**\", \"/images/**\", \"/listar\"").permitAll()
+					.anyRequest().authenticated())
+					.formLogin(formLogin -> formLogin.successHandler(successHandler).loginPage("/login")
 
-					
-	            .rememberMe(Customizer.withDefaults());
-	        ;
-	        
-	        return http.build();
-	    }
+							.permitAll())
+
+					.exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/error_403"))
+
+					.logout((logout) -> logout.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()))
+					// .logout((logout) -> logout.logoutUrl("/my/success/endpoint"))
+
+					.rememberMe(Customizer.withDefaults());
+			;
+
+			return http.build();
+		}
 	}
 
 	@Autowired
 	private LoginSuccesHandler successHandler;
-	
 
-/*
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((authorize) ->
+	/*
+	 * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http)
+	 * throws Exception { http.authorizeHttpRequests((authorize) ->
+	 * 
+	 * authorize.requestMatchers("/", "/css/**", "/js/**", "/images/**",
+	 * "/listar").permitAll() /*
+	 * .requestMatchers("/uploads/**").hasRole("USER").requestMatchers("/ver/**").
+	 * hasRole("USER")
+	 * .requestMatchers("/factura/**").hasRole("ADMIN").requestMatchers("/form/**").
+	 * hasRole("ADMIN") .requestMatchers("/eliminar/**").hasRole("ADMIN")
+	 */
+	// .requestMatchers("/").authenticated().anyRequest().denyAll()
+	// .successHandler(successHandler)
+	// .loginPage("/login")
+	// .permitAll()
+	// );
 
-		authorize.requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-				/*
-				 * .requestMatchers("/uploads/**").hasRole("USER").requestMatchers("/ver/**").
-				 * hasRole("USER")
-				 * .requestMatchers("/factura/**").hasRole("ADMIN").requestMatchers("/form/**").
-				 * hasRole("ADMIN") .requestMatchers("/eliminar/**").hasRole("ADMIN")
-				 */
-				//.requestMatchers("/").authenticated().anyRequest().denyAll()
-				//.successHandler(successHandler)
-				//.loginPage("/login")
-				//.permitAll()
-	//	);
+	// return http.build();
 
-		//return http.build();
-
-	//} */
+	// } */
 
 }
